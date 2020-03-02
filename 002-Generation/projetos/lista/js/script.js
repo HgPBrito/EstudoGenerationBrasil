6115e711062bd801a72188835a7feab49a5b7e4f
HUGO PEREIRA DE BRITO <hugo.pbrito@senacsp.edu.br>
2020-03-02 16:00:40 -0300

    Inicio relogio

--- 002-Generation/projetos/lista/js/script.js
+++ 002-Generation/projetos/lista/js/script.js
@@ -1,14 +1,21 @@
+let listaDeItens = ['Chuchu', 'Abacaxi', 'Cacau'];
+let listElement = document.querySelector('ul');
+let inputElement = document.querySelector('input');
+
 function addItem(text) {
     let liElement = document.createElement('li');
     let textElement = document.createTextNode(text);
     liElement.appendChild(textElement);
+    listElement.appendChild(liElement);
 }
 
-let nomes = ['Chuchu', 'Abacaxi', 'Cacau'];
-let listElement = document.querySelector('ul');
-let inputElement = document.querySelector('input');
 
 document.getElementById('btn').addEventListener('click', () => {
     addItem(inputElement.value);
+    listaDeItens.push(inputElement.value);
     inputElement.value = '';
 });
+
+for (item of listaDeItens) {
+    addItem(item);
+}
---
+++ 002-Generation/projetos/relogio/js/timer.js
@@ -0,0 +1,2 @@
+let currentTime = document.getElementById('currtime');
+
