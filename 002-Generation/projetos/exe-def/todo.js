5b38cd0325a6042d64d9da4912c1fb5fe1946c94
HUGO PEREIRA DE BRITO <hugo.pbrito@senacsp.edu.br>
2020-03-05 14:17:28 -0300

    Todo List

--- 002-Generation/projetos/exe-def/todo.js
+++ 002-Generation/projetos/exe-def/todo.js
@@ -1,15 +1,17 @@
-var listaDeTodos = [];
+let todoList = new Array();
 
 function addTodoItem(todo) {
-    var ul = document.getElementById("todosList");
-    var li = document.createElement("li");
+    let ul = document.getElementById("todosList");
+    let li = document.createElement("li");
     li.className = "list-group-item";
     li.appendChild(document.createTextNode(todo.description + " - " + todo.dueDate));
     ul.appendChild(li);
+    todoList.push(todo);
+
 }
 
 function showErrorMessage(message) {
-    var alert = document.getElementById("alert");
+    let alert = document.getElementById("alert");
     alert.innerHTML = message;
     alert.style.visibility = "visible";
 }
@@ -18,72 +20,41 @@ function hideErrorMessage() {
     document.getElementById("alert").style.visibility = "hidden";
 }
 
-function onAddClicked() {
-    var description = document.getElementById("description").value;
-    var dueDate = document.getElementById("dueDate").value;
-
-    let novadt = new Date(dueDate);
-    novadt.getDate() + '/'
-
-    // dueDate = dueDate.split("-").reverse().join(" \/ ");
-
-    var todo = new Todo(description, dueDate);
-
-    addTodoItem(todo);
-    listaDeTodos.push(todo);
-
-}
-
 function validTodo(todo) {
-}
-
-function clearTodos() {
-
-
+    if (todo.description.length === 0) {
+        showErrorMessage("Descrição Obrigatória");
+    } else if (todo.dueDate.length === 0) {
+        showErrorMessage("Data Obrigatória");
+    } else {
+        hideErrorMessage();
+        return true;
+    }
+    return false;
 }
 
 function Todo(description, dueDate) {
-    this.description = description;
-    this.dueDate = dueDate;
+    return {
+        "description": description,
+        "dueDate": dueDate
+    };
 }
 
+document.getElementById('btn-add').addEventListener('click', () => {
+    let description = document.getElementById("description").value;
+    let dueDate = document.getElementById("dueDate").value;
+    dueDate = dueDate.split("-").reverse().join(" \/ ");
 
+    let todo = Todo(description, dueDate);
 
-
-
-function validateForm() {
-
-    let email = document.getElementById('email').value;
-    let senha = document.getElementById('password').value;
-
-    if (email.length < 1) {
-        document.getElementById("alert-email").innerHTML = "Invalid input field. Email should have at least 1 character!";
-        document.getElementById("alert-email").style.visibility = "visible";
-    } else if (email.includes('@')) {
-        let terminacoes = [".com", ".co", ".org", ".net", ".br"];
-        let tem = false;
-        for (let i = 0; i < terminacoes.length; i++) {
-            if (email.endsWith(terminacoes[i])) {
-                tem = true;
-            }
-        }
-
-        if (tem) {
-            document.getElementById("alert-email").style.visibility = "hidden";
-        } else {
-            document.getElementById("alert-email").innerHTML = "Invalid input field. Termination of invalid Email!";
-            document.getElementById("alert-email").style.visibility = "visible";
-        }
-    } else {
-        document.getElementById("alert-email").innerHTML = "Invalid input field. Email Invalid!";
-        document.getElementById("alert-email").style.visibility = "visible";
+    if (validTodo(todo)) {
+        addTodoItem(todo);
     }
+    console.log(todoList);
 
+});
+document.getElementById('btn-remove').addEventListener('click', () => {
+    let ul = document.getElementById('todosList');
+    ul.innerHTML = '';
+    todoList = [];
 
-    if (senha.length < 1) {
-        document.getElementById("alert-password").innerHTML = "Invalid input field. Password should have at least 1 character!";
-        document.getElementById("alert-password").style.visibility = "visible";
-    } else {
-        document.getElementById("alert-password").style.visibility = "hidden";
-    }
-}
+});
